50953de455e93ce0b87503d6658cdd5419b48a8c
Mason Donahue <masond+github@gmail.com>
2014-09-28 09:02:07 -0500

    I'm pretty sure this actually works.

--- reverseproxy.py
+++ reverseproxy.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, make_response, request
+import requests
 import urllib2
+import urllib
 import json
+import pdb
 
 jenkins_url = "http://mlearn.sshchicago.org:8080"
-
+needs_crumb = False
 
 app = Flask(__name__)
 
@@ -14,26 +17,20 @@ def hello():
 
 @app.route("/github-webhook/", methods = ['POST'])
 def handle_webhook():
-    hook_json = request.json
-    print hook_json
-    crumb_request = urllib2.Request(jenkins_url + "/crumbIssuer/api/json")
-    crumb_response = urllib2.urlopen(crumb_request)
-    csrf_crumb = json.loads(crumb_response.read())
-    print "Crumb received: " + csrf_crumb['crumbRequestField'] + csrf_crumb['crumb']
-    proxied_request = urllib2.Request(jenkins_url + '/github-webhook/')
-    proxied_request.add_header('Content-Type', 'application/json')
-#    proxied_request.add_header(csrf_crumb['crumbRequestField'], csrf_crumb['crumb'])
-    jenkins_response = urllib2.urlopen(proxied_request, json.dumps(hook_json))
-    print jenkins_response
-    return jenkins_response.json
+    hook = request.form['payload']
+    print hook
+    if needs_crumb:
+        crumb_request = requests.get(jenkins_url + "/crumbIssuer/api/json")
+        csrf_crumb = crumb_request.json()
+        print "Crumb received: " + csrf_crumb['crumbRequestField'] + csrf_crumb['crumb']
+    data = [('payload', hook)]
+    jenkins_response = requests.post(jenkins_url + "/github-webhook/", data=data )
+    print jenkins_response.status_code
+    return jenkins_response.text
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
-
-
-
+    app.run(host='0.0.0.0')
