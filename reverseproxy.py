e6475bd6c997b10ae9616a871e1fc6a86f13131a
Mason Donahue <masond+github@gmail.com>
2014-09-28 09:25:13 -0500

    Functioning.

--- reverseproxy.py
+++ reverseproxy.py
@@ -5,11 +5,11 @@ import urllib
 import json
 import pdb
 
-jenkins_url = "http://mlearn.sshchicago.org:8080"
+jenkins_url = "http://your.jenkins.instance:8080"
 needs_crumb = True
+debug = False
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
@@ -18,14 +18,17 @@ def hello():
 @app.route("/github-webhook/", methods = ['POST'])
 def handle_webhook():
     hook = request.form['payload']
-    print hook
+    if debug:
+        print hook
     if needs_crumb:
         crumb_request = requests.get(jenkins_url + "/crumbIssuer/api/json")
         csrf_crumb = crumb_request.json()
-        print "Crumb received: " + csrf_crumb['crumbRequestField'] + csrf_crumb['crumb']
+        if debug:
+            print "Crumb received: " + csrf_crumb['crumbRequestField'] + "=" + csrf_crumb['crumb']
     data = [('payload', hook)]
     jenkins_response = requests.post(jenkins_url + "/github-webhook/", data=data )
-    print jenkins_response.status_code
+    if jenkins_response.status_code != 200:
+        print "ERROR: Jenkins returned " + jenkins_response.status_code
     return make_response(jenkins_response.text, jenkins_response.status_code)
 
 @app.errorhandler(404)
